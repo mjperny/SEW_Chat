dd8f73b1f0c184f33c3821cbceb4079c017edd61
mgoebel-tgm <melanie.goebel@student.tgm.ac.at>
2014-12-10 11:28:39 +0100

    Badwords not casesensitive anymore

--- src/goebelkronowetter/BadWords.java
+++ src/goebelkronowetter/BadWords.java
@@ -1,42 +1,68 @@
 package goebelkronowetter;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
-import javax.swing.JOptionPane;
+public class BadWords extends Decorator{
+	private ArrayList<String> ch = new ArrayList<String>();
+	private FileReader fr;
+	private BufferedReader br;
 
-public class BadWords extends Decorator {
-	private String badwords;
-	
-	public BadWords(Chat chat){
-		super(chat);
-		badwords = "";
-		readBadWords();
+	public BadWords(Chat c) {
+		super(c);
+		try{
+			fr = new FileReader("data/BadWords.txt");
+			br = new BufferedReader(fr);
+			ch.add("*");
+			ch.add("$");
+			ch.add("+");
+			ch.add("?");
+			ch.add("&");
+			ch.add("§");
+		} catch (IOException e) {
+			System.err.println("File nicht gefunden!");
+		}
 	}
-	
 	@Override
 	public String writeMessage(String message) {
-		return message.replaceAll(badwords, "******");
-	}
-	
-	private void readBadWords(){
+		String[] words = message.split(" ");
+		String censorMessage = "";
 		try{
-		File f = new File("data/badwords.txt");
-		BufferedReader in = new BufferedReader(new FileReader(f));
-		String s;
-		while ((s = in.readLine()) != null){
-			badwords += s + "|";
+			for(int i = 0; i < words.length; i++){
+				fr = new FileReader("data/BadWords.txt");
+				br = new BufferedReader(fr);
+				censorMessage += censorWord(words[i])+" ";
+			}
+		} catch (IOException e) {
+			System.err.println("File nicht gefunden!");
 		}
-		badwords = badwords.substring(0, badwords.length()-1);
-		in.close();
-		}catch(FileNotFoundException fnfe){
-			JOptionPane.showMessageDialog(null, "File not found! It has to be in the directory data and named badwords.txt");
-		}catch(IOException ioe){
-			JOptionPane.showMessageDialog(null, "Something went wrong with the file badwords.txt");
-		}
+		return censorMessage;
 	}
-	
+	public String censorWord(String word){
+		String censorWord = "";
+		try{
+			boolean endloop = false;
+			while(endloop == false){
+				String zeile1 = br.readLine(); 
+				if(zeile1 == null)
+					endloop = true;
+				else if((word.toUpperCase()).equals(zeile1.toUpperCase())){
+					for(int i = 0; i < word.length(); i++){
+						censorWord += ch.get((int) (Math.random()*ch.size()));
+					}
+					br.close();
+					fr.close();
+					return censorWord;
+				}
+
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		return word;
+	}
+
 }
